494e186919b15df25232a4334e6ee93082a70766
shinyay <shinya.com@gmail.com>
2025-02-27 15:26:01 +0900

    Add sample COBL application

---
+++ src/hello.cob
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, COBOL Development Container!".
+           DISPLAY "This is a sample COBOL program.".
+           STOP RUN.
