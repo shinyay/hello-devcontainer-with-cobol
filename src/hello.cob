94249f2f8e2487686b988c2fdb88a848e5e569a0
shinyay <shinya.com@gmail.com>
2025-02-27 17:06:24 +0900

    Modify COBOL format

--- src/hello.cob
+++ src/hello.cob
@@ -1,11 +1,9 @@
 IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
-
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
            CONSOLE IS DISPLAY-DEVICE.
-
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 WS-CURRENT-DATE.
@@ -13,20 +11,17 @@ IDENTIFICATION DIVISION.
           05 WS-MONTH               PIC 9(2).
           05 WS-DAY                 PIC 9(2).
        01 WS-FORMATTED-DATE         PIC X(20).
-
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            PERFORM DISPLAY-WELCOME
            PERFORM GET-AND-DISPLAY-DATE
            PERFORM PROGRAM-DONE
            STOP RUN.
-
        DISPLAY-WELCOME.
            DISPLAY "********************************"
            DISPLAY "* COBOL Development Container! *"
            DISPLAY "********************************"
            DISPLAY SPACE.
-
        GET-AND-DISPLAY-DATE.
            MOVE FUNCTION CURRENT-DATE(1:8) TO WS-CURRENT-DATE
            STRING "Date: "
@@ -37,7 +32,6 @@ IDENTIFICATION DIVISION.
                   WS-DAY DELIMITED BY SIZE
                   INTO WS-FORMATTED-DATE
            DISPLAY WS-FORMATTED-DATE.
-
        PROGRAM-DONE.
            DISPLAY SPACE
            DISPLAY "Program execution completed.".
